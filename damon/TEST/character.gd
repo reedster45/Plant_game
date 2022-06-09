a05494395e4fa26da667357f3ee3eab79421da48
Reed Tyhuis <tyhuis34@gmail.com>
2022-06-09 15:20:42 -0400

    set character idle to frame 0

--- damon/TEST/character.gd
+++ damon/TEST/character.gd
@@ -14,7 +14,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	check_idle()
 
 
@@ -31,7 +31,7 @@ func check_idle():
 	if moving:
 		$AnimatedSprite.play(direction)
 	else:
-		$AnimatedSprite.stop()
+		$AnimatedSprite.frame = 0
 
 
 func movement_input():
